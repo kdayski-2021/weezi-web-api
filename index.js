9b4653fcfb7fd09959abf44edc86a37fc59db237
fanilus <fanil@fanil1.fvds.ru>
2021-11-16 13:16:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const dotenv = require('dotenv')
+const express = require('express')
+const cors = require('cors')
+const app = new express()
+app.use(cors())
+dotenv.config()
+
+const googleUrl = '1bX-db7LGhGMwocvpr_0izm6zWXyrjw4rlTb50Qt0SMA'
+async function loadData() {
+    const doc = new GoogleSpreadsheet(googleUrl);
+    doc.useApiKey(process.env.GOOGLE_API_KEY);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.loadCells('A1:J30')
+    let resultRowIndex
+    let daos = []
+    for (i = 2; i < 30; i++) {
+        const daoName = await sheet.getCell(i, 1)
+        if (daoName.value === 'ИТОГО') {
+            resultRowIndex = i
+            break
+        } else {
+            daos.push(daoName.value)
+        }
+    }
+    const currentUsers = sheet.getCell(resultRowIndex, 3).value
+    const aum = sheet.getCell(resultRowIndex, 4).value
+    const totalTransactionVolume = sheet.getCell(resultRowIndex, 5).value
+    const votesCastByCommunity = sheet.getCell(resultRowIndex, 9).value
+
+    const daoInfo = {
+        DAOVaults: daos.length,
+        currentUsers,
+        aum: Math.round(aum / 100000) / 10,
+        totalTransactionVolume: Math.round(totalTransactionVolume / 100000) / 10,
+        votesCastByCommunity
+    }
+    return daoInfo
+}
+
+(async function () {
+    await loadData()
+}())
+
+app.use(express.json({ limit: '10mb' }))
+app.get('/dao-info', async (req, res) => {
+
+    res.status(200).send(await loadData())
+
+})
+let port = process.env.PORT || 30001
+app.listen(port, function () {
+    console.log("DAO API Server ready on port " + port)
+
+})
