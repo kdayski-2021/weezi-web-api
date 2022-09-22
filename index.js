d5c0ad8824466bcf3c4a537fd5a6e9da69152ea6
fanilus <demolit_fanil@mail.ru>
2022-09-22 11:21:33 +0300

    last

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function loadData() {
             daos.push(daoName.value)
         }
     }
+    
     const currentUsers = parseFloat(String(sheet.getCell(resultRowIndex, 3).value).replace(/ /g,"").replace(/,/g,"."))
     const aum = parseFloat(String(sheet.getCell(resultRowIndex, 4).value).replace(/ /g,"").replace(/,/g,"."))
     const totalTransactionVolume = parseFloat(String(sheet.getCell(resultRowIndex, 5).value).replace(/ /g,"").replace(/,/g,"."))
@@ -39,16 +40,60 @@ async function loadData() {
 
     return daoInfo
 }
+async function loadDataHistory() {
+    let data = []
+    const rows = 300
+    const doc = new GoogleSpreadsheet(googleUrl);
+    doc.useApiKey(process.env.GOOGLE_API_KEY);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.loadCells('A1:J'+rows)
+    
+    let resultRowIndex
+    let daos = []
+    for (i = 2; i < rows; i++) {
+        const daoName = await sheet.getCell(i, 1)
+        if (daoName.value === 'ИТОГО') {
+            resultRowIndex = i
+            const date =  sheet.getCell(resultRowIndex, 0).formattedValue
+            const currentUsers = parseFloat(String(sheet.getCell(resultRowIndex, 3).value).replace(/ /g,"").replace(/,/g,"."))
+            const aum = parseFloat(String(sheet.getCell(resultRowIndex, 4).value).replace(/ /g,"").replace(/,/g,"."))
+            const totalTransactionVolume = parseFloat(String(sheet.getCell(resultRowIndex, 5).value).replace(/ /g,"").replace(/,/g,"."))
+            const votesCastByCommunity = parseFloat(String(sheet.getCell(resultRowIndex, 9).value).replace(/ /g,"").replace(/,/g,"."))
+
+            const daoInfo = {
+                date,
+                DAOVaults: daos.length,
+                currentUsers,
+                aum: Math.round(aum / 100000) / 10,
+                aumS: String(sheet.getCell(resultRowIndex, 4).value).replace(/ /g,""),
+                totalTransactionVolume: Math.round(totalTransactionVolume / 100000) / 10,
+                totalTransactionVolumeS: String(sheet.getCell(resultRowIndex, 5).value).replace(/ /g,""),
+                votesCastByCommunity
+            }
+            daos = []
+            data.push(daoInfo)
+        } else {
+            daos.push(daoName.value)
+        }
+    
+    
+    }
+
+
+    return data
+}
 
 (async function () {
-    await loadData()
+    console.log(await loadDataHistory())
 }())
 
 app.use(express.json({ limit: '10mb' }))
 app.get('/dao-info', async (req, res) => {
-
     res.status(200).send(await loadData())
-
+})
+app.get('/dao-history', async (req, res) => {
+    res.status(200).send(await loadDataHistory())
 })
 let port = process.env.PORT || 30001
 app.listen(port, function () {
